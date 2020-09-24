84432d226f61854ac218899ebd4526973088949c
quzhongling <quzhongling@dm-ai.cn>
2020-09-24 09:54:34 +0800

    修改gin启动方式

--- src/common/server/server.go
+++ src/common/server/server.go
@@ -1,15 +1,10 @@
 package server
 
 import (
-	"context"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
 	"runtime"
-	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Run(router *gin.Engine, port string, debug bool) {
@@ -24,31 +19,33 @@ func Run(router *gin.Engine, port string, debug bool) {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
+	router.Run(":" + port)
 
-	go func() {
-		// service connections
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
+	// srv := &http.Server{
+	// 	Addr:    ":" + port,
+	// 	Handler: router,
+	// }
 
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 10 seconds.
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-	log.Println("Shutdown Server ...")
+	// go func() {
+	// 	// service connections
+	// 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	// 		log.Fatalf("listen: %s\n", err)
+	// 	}
+	// }()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
-	log.Println("Server exiting")
+	// // Wait for interrupt signal to gracefully shutdown the server with
+	// // a timeout of 10 seconds.
+	// quit := make(chan os.Signal)
+	// signal.Notify(quit, os.Interrupt)
+	// <-quit
+	// log.Println("Shutdown Server ...")
+
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// if err := srv.Shutdown(ctx); err != nil {
+	// 	log.Fatal("Server Shutdown:", err)
+	// }
+	// log.Println("Server exiting")
 
 	//pid := fmt.Sprintf("%d", os.Getpid())
 	//_, openErr := os.OpenFile("pid", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
