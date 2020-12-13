497a38af1c64ce74a5be9792d50fb46bfc61f25d
liaolonglong <liaolonglong@dm-ai.cn>
2020-12-13 15:35:10 +0800

    优化result结果查询功能

--- src/backend-service/service/deploy.go
+++ src/backend-service/service/deploy.go
@@ -9,9 +9,12 @@ func GetDeployByResult(results ...model.Result) (deploys []model.Deploy) {
 	l := len(results)
 	wheres, args := make([]string, l), make([]interface{}, 0)
 	if l > 0 {
+		//  result version传入的是0.0.0没有意义
 		for i, result := range results {
-			wheres[i] = "(name = ? and env_id = ? and version = ?)"
-			args = append(args, result.Name, GetEnvIDByName(result.DeployEnv), result.Version)
+			//wheres[i] = "(name = ? and env_id = ? and version = ?)"
+			//args = append(args, result.Name, GetEnvIDByName(result.DeployEnv), result.Version)
+			wheres[i] = "(name = ? and env_id = ?)"
+			args = append(args, result.Name, GetEnvIDByName(result.DeployEnv))
 		}
 		model.DB.Where(strings.Join(wheres, " and "), args...).Find(&deploys)
 	}
